a607b18df8fae82f2f848834e908958cd8c5fa2a
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-05-26 22:08:50 +0800

    Add main testing routine

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ width = 7
 
 # Num of pawns in a line to win
 connect = 4
+
+# Play config
+turn = True # True to go first, False to go second
+time_control = 1 # Time control, in seconds
---
+++ main.py
@@ -0,0 +1,41 @@
+from algo import Algo
+from board import Board, State
+from config import turn, width, time_control
+
+def main():
+    board = Board()
+    algo = Algo()
+    while board.winner == State.UNDETERMINED:
+        print(board)
+        if board.is_X_turn == turn:
+            # human turn
+            response = input("Your turn: ")
+            while True:
+                try:
+                    response = int(response) - 1
+                    if response < 0 or response >= width:
+                        response = input("Invalid move, try again: ")
+                    break
+                except ValueError:
+                    response = input("Not an integer, try again: ")
+            
+            board = board.move(response)
+        
+        else:
+            # algo turn
+            move = algo.next_move(board, time_control)
+            print(f"Algo choose: {move + 1}")
+            board = board.move(move)
+    
+    print(board)
+    if board.winner == State.DRAW:
+        print("Draw!")
+    else:
+        is_X_win = board.winner == State.X
+        if is_X_win == turn:
+            print("You win!")
+        else:
+            print("Algo win!")
+
+if __name__ == '__main__':
+    main()
